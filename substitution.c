5ab60c3457ebdad261192a0a4f71710aa56e2728
acpejo <acpejo@users.noreply.github.com>
2020-01-17 21:50:01 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ substitution.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc , string argv[])
+{
+    if(argc !=2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    if(strlen(argv[1])!=26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+}
