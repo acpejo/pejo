7bd090f1f106f86b48ac0d1a79c80f6d8fcf265b
acpejo <acpejo@users.noreply.github.com>
2020-01-17 22:50:21 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- substitution.c
+++ substitution.c
@@ -3,56 +3,60 @@
 #include <string.h>
 #include <ctype.h>
 
-int main (int argc , string argv[])
+int main(int argc, string argv[])
 {
     char A[26];
-    if(argc !=2)
+    if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    if(strlen(argv[1])!=26)
+    if (strlen(argv[1]) != 26)
     {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
-    for(int i=0; i<26; i++)
+    for (int i = 0; i < 26; i++)
     {
-        if(! isupper(argv[1][i]) && ! islower(argv[1][i]))
+        if (! isupper(argv[1][i]) && ! islower(argv[1][i]))
         {
             printf("Usage: ./substitution key\n");
             return 1;
         }
     }
-    for(int i=0; i<26; i++)
+    for (int i = 0; i < 26; i++)
     {
-        for(int j=0;j<26;j++)
+        for(int j = 0; j < 26; j++)
         {
-            if (i==j)
+            if (i == j)
+            {
                 continue;
-            if(argv[1][i]==toupper(argv[1][j]) || argv[1][i]==tolower(argv[1][j]))
+            }
+            if( argv[1][i] == toupper(argv[1][j]) || argv[1][i] == tolower(argv[1][j]))
             {
                 printf("Usage: ./substitution key\n");
                 return 1;
             }
         }
     }
-    string plain=get_string("plaintext:  ");
+    string plain = get_string("plaintext:  ");
     printf("ciphertext: ");
     char cipher[strlen(plain)];
-    for(int i=0;i<strlen(plain);i++)
+    for (int i =0; i < strlen(plain); i++)
     {
         if(! isupper(plain[i]) && ! islower(plain[i]))
         {
-            cipher[i]=plain[i];
+            cipher[i] = plain[i];
             continue;
         }
         else if(isupper(plain[i]))
         {
-            cipher[i]=toupper(argv[1][plain[i]-65]);
+            cipher[i] = toupper(argv[1][plain[i] - 65]);
         }
         else //plain is lower
-            cipher[i]=tolower(argv[1][plain[i]-97]);
+        {
+            cipher[i] = tolower(argv[1][plain[i] - 97]);
+        }
     }
-    printf("%s\n",cipher);
+    printf("%s\n", cipher);
 }
