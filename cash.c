b9bf99d681971638eeda4f9ede818c73dcdf4d38
acpejo <acpejo@users.noreply.github.com>
2020-01-18 01:19:59 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -13,22 +13,22 @@ int main(void)
     int k = 0, money = round(mon * 100);
     if (money >= 25)
     {
-        k = money / 25;
+        k += money / 25;
         money %= 25;
     }
     if (money >= 10)
     {
-        k = money / 10;
+        k += money / 10;
         money %= 10;
     }
     if (money >= 5)
     {
-        k = money / 5;
+        k += money / 5;
         money %= 5;
     }
     if (money >= 1)
     {
-        k = money ;
+        k += money ;
     }
     printf("%i\n", k);
 }
