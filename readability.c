993d3f34107c204741434821c6c53b349d839c3e
acpejo <acpejo@users.noreply.github.com>
2020-01-24 16:09:15 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ readability.c
@@ -0,0 +1,42 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    string s = get_string("Text: ");
+    int l = 0, S = 0, w = 1;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == ' ')
+        {
+            w++;
+        }
+        else if (s[i] == '!' || s[i] == '?' || s[i] == '.')
+        {
+            S++;
+        }
+        else if (isupper(s[i]) || islower(s[i]))
+        {
+            l++;
+        }
+    }
+    l = l * 100 / w;
+    S = S * 100 / w;
+    float index = 0.0588 * l - 0.296 * S - 15.8;
+    int p = round(index);
+    if (p < 1)
+    {
+        printf("%s\n", "Before Grade 1");
+    }
+    else if (p >= 16)
+    {
+        printf("%s\n", "Grade 16+");
+    }
+    else
+    {
+        printf("%s%i\n", "Grade ", p);
+    }
+}
