d864967588f1ec3e598208d856b954ea7b9daedd
acpejo <acpejo@users.noreply.github.com>
2020-05-05 14:28:22 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -1,71 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdbool.h>
-
+#include <stdint.h>
+#include <stdlib.h>
 
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-
-    //Ensure that the user enter the correct name at promt
+    //enure that the user input two strings
     if (argc != 2)
     {
         printf("Usage: ./recover image\n");
         return 1;
     }
 
-    //Open file
-    FILE *file = fopen(argv[1], "r");
+    //the main file of deleted memory
+    FILE *inptr = fopen(argv[1], "r");
 
-    //Ensure that the file is 100 100
-    if (file == NULL)
+    //enusre that every thing is ok
+    if (inptr == NULL)
     {
-        return 1;
+        return 2;
     }
 
-    BYTE *buffers = malloc(512 * sizeof(BYTE));
-    int count = 0;
-
-    char *filename = malloc(8 * sizeof(char));
-
+    //The output files of the recoverd data
     FILE *outptr = NULL;
 
+    //pointer to rewrite the data
+    char *temp = malloc(8 * sizeof(char));
+
+    //pointer to read the data
+    BYTE *buffer = malloc(512 * sizeof(BYTE));
+
+    short counter = 0;
 
     while (true)
     {
-        size_t byteRead = fread(buffers, sizeof(BYTE), 512, file);
+        //read a block of memory from a card image
+        size_t fileRead = fread(buffer, sizeof(BYTE), 512, inptr);
 
-        if (byteRead == 0 && feof(file))
+        //flag to know wiether the pointer is jpg or not
+        bool jpg = buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+
+        //Ending form one image and start new one
+        if (jpg && outptr != NULL)
+        {
+            fclose(outptr);
+            counter++;
+        }
+
+        //All is done
+        if (fileRead < 512)
         {
             break;
         }
 
-        bool containJpegHeader = buffers[0] == 0xff && buffers[1] == 0xd8 && buffers[2] == 0xff && (buffers[3] & 0xf0) == 0xe0;
-
-        if (containJpegHeader && outptr != NULL)
+        //write data to file
+        if (jpg)
         {
-            fclose(outptr);
-            count++;
-        }
-
-        if (containJpegHeader)
-        {
-            sprintf(filename,"%03i.jpg", count);
-            outptr = fopen(filename, "w");
-
+            sprintf(temp, "%03i.jpg", counter);
+            outptr = fopen(temp, "w");
         }
 
         if (outptr != NULL)
         {
-            fwrite(buffers, sizeof(BYTE), byteRead, outptr);
+            fwrite(buffer, sizeof(BYTE), fileRead, outptr);
         }
+
     }
 
+    fclose(inptr);
     fclose(outptr);
-    fclose(file);
+    free(buffer);
+    free(temp);
 
-    free(buffers);
-    free(filename);
 }
